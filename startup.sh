c7eb6e74729ee8cc911031a2c1e79372f72471a7
Star Liu <starliu.sstx@gmail.com>
2024-11-03 23:59:21 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 # startup.sh
 # Install necessary system libraries for WeasyPrint
-apt install weasyprint
+apt install python3-pip libpango-1.0-0 libpangoft2-1.0-0 libjpeg-dev libopenjp2-7-dev libffi-dev libgobject-2.0-0
 
 # Start the Gunicorn server
 gunicorn --bind=0.0.0.0:8000 wsgi:server
